f1933e9f39ee93f69f980d24b509235186a120c2
leviathanwhite <2843523795@qq.com>
2024-01-20 21:03:23 +0800

    Update scrpt.cs

--- Assets/ScriptableObjects/scrpt.cs
+++ Assets/ScriptableObjects/scrpt.cs
@@ -7,7 +7,7 @@ public class scrpt : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        float a = 0;
     }
 
     // Update is called once per frame
